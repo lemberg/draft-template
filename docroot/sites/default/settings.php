726d7954df0730e9c0dd0af0026915c0ec2ac404
Roman Paska <roman.paska@gmail.com>
2017-06-28 12:29:28 +0300

    Fix incorrect path to the config sync directory in settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -3,7 +3,7 @@
 require_once 'default.settings.php';
 
 // This is defined inside the read-only "config" directory, deployed via Git.
-$config_directories[CONFIG_SYNC_DIRECTORY] = $app_root . '../config/' . basename($site_path);
+$config_directories[CONFIG_SYNC_DIRECTORY] = $app_root . '/../config/' . basename($site_path);
 
 // Installation profile.
 $settings['install_profile'] = 'draft';

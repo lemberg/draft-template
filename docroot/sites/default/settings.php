e35469434dcaa198766e28ed27cf0225814a8f0b
Roman Paska <roman.paska@lemberg.co.uk>
2017-12-13 17:00:08 +0200

    Include Platform.sh settings if available

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -8,6 +8,11 @@ $config_directories[CONFIG_SYNC_DIRECTORY] = $app_root . '/../config/' . basenam
 // Installation profile.
 $settings['install_profile'] = 'draft';
 
+// Include Platform.sh settings if available.
+if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
+  include $app_root . '/' . $site_path . '/settings.platformsh.php';
+}
+
 // Local settings. These come last so that they can override anything.
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
